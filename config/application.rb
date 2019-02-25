35d902767f244bfc576267dbe74202f56687e8a9
Mario Salles <mariofsalles@gmail.com>
2019-02-25 13:37:32 +0000

    Scenario: Register one customer

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,9 @@ module TddApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    #I18N - pt:BR
+    config.i18n.default_locale = :'pt-BR'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -19,4 +19,20 @@ RSpec.feature "Customers", type: :feature do
     click_on('New Customer')
     expect(page).to have_content('New Customer')
   end
+
+  scenario 'Register one customer' do
+
+    visit(new_customer_path)
+    customer_name = Faker::Name.name
+    fill_in('Name', with: customer_name)
+    fill_in('Email', with: Faker::Internet.email)
+    fill_in('Phone', with: Faker::PhoneNumber.phone_number)
+    attach_file('Avatar', "#{Rails.root}/spec/fixtures/avatar.png")
+    choose(option: ['Y','N'].sample)
+    click_on('Create Account')
+    
+    expect(page).to have_content('Customer registered!')
+    expect(Customer.last.name).to eq(customer_name)
+    end
+
 end
