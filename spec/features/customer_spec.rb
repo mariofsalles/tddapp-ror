7e211bfe36da71ba2b2779d288cdbcecb543e4ed
Mario Salles <mariofsalles@gmail.com>
2019-02-26 00:11:50 +0000

    Scenario: Testing links

--- spec/features/customer_spec.rb
+++ spec/features/customer_spec.rb
@@ -93,7 +93,35 @@ RSpec.feature "Customers", type: :feature do
 
     expect(page).to have_content('Customer updated complete!')
     expect(page).to have_content(new_name)
-
   end
 
+  scenario 'Testing show-link' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['Y','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, '/html/body/table/tbody/tr[1]/td[2]/a').click
+    expect(page).to have_content('Show Customer')
+  end
+
+
+  scenario 'Testing edit-link' do
+    customer = Customer.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['Y','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, '/html/body/table/tbody/tr[1]/td[3]/a').click
+    expect(page).to have_content('Customer edit')
+  end
 end
+
